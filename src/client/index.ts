e6c8321cff2f97c9416cb1ea354da620b6ebf410
Alcione <alcionefribeiro@gmail.com>
2022-06-24 15:24:37 +0200

    encoding category query still not finished.

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,6 +3,21 @@ const axios = require('axios');
 async function main() {
     console.log(" ------ =^.^= ---- Top Influencer ------ =^.^= ----");
     console.log(await getTopInfluencer());
+    console.log("------ =^.^= ----")
+
+    console.log(" ------ =^.^= ---- Top Influencer in category Lifestyle------ =^.^= ----");
+    console.log(await getTopInfluencerInCategory("Lifestyle"));
+    console.log("------ =^.^= ----")
+
+    console.log(" ------ =^.^= ---- Top Influencer in category Cinema & Actors/actresses------ =^.^= ----");
+    console.log(await getTopInfluencerInCategory("Cinema & Actors/actresses"));
+    console.log("------ =^.^= ----")
+
+    console.log(" ------ =^.^= ---- Top Influencer in category Humor & Fun & Happiness------ =^.^= ----");
+    console.log(await getTopInfluencerInCategory("Humor & Fun & Happiness"));
+    console.log("------ =^.^= ----")
+
+
 }
 
 
@@ -12,5 +27,15 @@ async function getTopInfluencer() {
         resolve(`${response.data["username"]} with ${response.data["followers"]} followers`);
     });
 }
+
+
+async function getTopInfluencerInCategory(category: string) {
+    let encodeQuery = encodeURI(category)
+    let response = await axios.get(`http://localhost:3000/top-influencer-per-category?category=${encodeQuery}`);
+    return new Promise((resolve) => {
+        resolve(`${response.data["username"]} with ${response.data["followers"]} followers`);
+    });
+}
+
 
 main().then();
--- src/server/index.ts
+++ src/server/index.ts
@@ -14,8 +14,28 @@ async function main() {
                                    replace(replace(replace(Followers, '.', ''), 'M', '0000'), 'K', '000')
                                as INTEGER)           as no_followers
                     from influencers
+                    order by no_followers desc LIMIT 1;`, (err: Error, row: any) => {
+                res.send({
+                    "username": row["username"],
+                    "followers": row["no_followers"]
+                });
+            });
+        });
+    })
+
+    // http://localhost:3000/top-influencer-per-category?category=Lifestyle
+    app.get('/top-influencer-per-category', (req: Request, res: Response) => {
+        let category = req.query['category'];
+        db.serialize(() => {
+            db.get(`Select \`Influencer insta name\` as username,
+                           cast(
+                                   replace(replace(replace(Followers, '.', ''), 'M', '0000'), 'K', '000')
+                               as INTEGER)           as no_followers
+                    from influencers
+                    WHERE category_1 = ?
+                       or category_2 = ?
                     order by no_followers desc
-                    LIMIT 1;`, (err: Error, row: any) => {
+                        LIMIT 1;`, category, category, (err: Error, row: any) => {
                 res.send({
                     "username": row["username"],
                     "followers": row["no_followers"]
