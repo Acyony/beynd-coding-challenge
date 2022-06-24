7407b09cd3f61d6d8f0a4eebd7bd62fa9b35bb0f
Alcione <alcionefribeiro@gmail.com>
2022-06-24 18:41:56 +0200

    Last commit.

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,28 +1,38 @@
 const axios = require('axios');
 
 async function main() {
-    console.log(" ------ =^.^= ---- Top Influencer ------ =^.^= ----");
-    console.log(await getTopInfluencer());
-    console.log("------ =^.^= ----")
+    try {
+        console.log(" ------ =^.^= ---- Top Influencer ------ =^.^= ----");
+        console.log(await getTopInfluencer());
+        console.log("------ =^.^= ----")
 
-    console.log(" ------ =^.^= ---- Top Influencer in category Lifestyle------ =^.^= ----");
-    console.log(await getTopInfluencerInCategory("Lifestyle"));
-    console.log("------ =^.^= ----")
+        console.log(" ------ =^.^= ---- Top Influencer in category Lifestyle------ =^.^= ----");
+        console.log(await getTopInfluencerInCategory("Lifestyle"));
+        console.log("------ =^.^= ----")
 
-    console.log(" ------ =^.^= ---- Top Influencer in category Cinema & Actors/actresses------ =^.^= ----");
-    console.log(await getTopInfluencerInCategory("Cinema & Actors/actresses"));
-    console.log("------ =^.^= ----")
+        console.log(" ------ =^.^= ---- Top Influencer in category Cinema & Actors/actresses------ =^.^= ----");
+        console.log(await getTopInfluencerInCategory("Cinema & Actors/actresses"));
+        console.log("------ =^.^= ----")
 
-    console.log(" ------ =^.^= ---- Top Influencer in category Humor & Fun & Happiness------ =^.^= ----");
-    console.log(await getTopInfluencerInCategory("Humor & Fun & Happiness"));
-    console.log("------ =^.^= ----")
+        console.log(" ------ =^.^= ---- Top Influencer in category Humor & Fun & Happiness------ =^.^= ----");
+        console.log(await getTopInfluencerInCategory("Humor & Fun & Happiness"));
+        console.log("------ =^.^= ----")
 
+        console.log(" ------ =^.^= ---- Top Influencer in country Brazil------ =^.^= ----");
+        console.log(await getTopInfluencerInCountry("Brazil"));
+        console.log("------ =^.^= ----")
+
+        console.log(" ------ =^.^= ---- Top Influencer in country India------ =^.^= ----");
+        console.log(await getTopInfluencerInCountry("India"));
+        console.log("------ =^.^= ----")
+    } catch (e) {
+        console.log(e);
+    }
 
 }
 
-
 async function getTopInfluencer() {
-    let response = await axios.get("http://localhost:3000/top-influencer");
+    let response = await axios.get("http://localhost:9595/top-influencer");
     return new Promise((resolve) => {
         resolve(`${response.data["username"]} with ${response.data["followers"]} followers`);
     });
@@ -30,9 +40,16 @@ async function getTopInfluencer() {
 
 
 async function getTopInfluencerInCategory(category: string) {
-    let encodeQuery = encodeURIComponent(category)
-    console.log(encodeQuery)
-    let response = await axios.get(`http://localhost:3000/top-influencer-per-category?category=${encodeQuery}`);
+    let encodeQuery = encodeURIComponent(category);
+    let response = await axios.get(`http://localhost:9595/top-influencer-per-category?category=${encodeQuery}`);
+    return new Promise((resolve) => {
+        resolve(`${response.data["username"]} with ${response.data["followers"]} followers`);
+    });
+}
+
+async function getTopInfluencerInCountry(country: string) {
+    let encodeCountry = encodeURIComponent(country);
+    let response = await axios.get(`http://localhost:9595/top-influencer-per-country?country=${encodeCountry}`);
     return new Promise((resolve) => {
         resolve(`${response.data["username"]} with ${response.data["followers"]} followers`);
     });
--- src/server/index.ts
+++ src/server/index.ts
@@ -4,9 +4,9 @@ import express, {Express, Request, Response} from 'express';
 async function main() {
     const db = new sqlite3.Database('data/instagram_influencers.sqlite3');
     const app: Express = express();
-    const port = 3000;
+    const port = 9595;
 
-    // http://localhost:3000/top-influencer
+    // http://localhost:9595/top-influencer
     app.get('/top-influencer', (req: Request, res: Response) => {
         db.serialize(() => {
             db.get(`Select \`Influencer insta name\` as username,
@@ -23,11 +23,10 @@ async function main() {
         });
     })
 
-    // http://localhost:3000/top-influencer-per-category?category=Lifestyle
+    // http://localhost:9595/top-influencer-per-category?category=Lifestyle
     app.get('/top-influencer-per-category', (req: Request, res: Response) => {
-        let category = req.query['category'];
+        let category = req.query['category'] as string;
         let decodedCategory = decodeURIComponent(category)
-        console.log(decodedCategory)
         db.serialize(() => {
             db.get(`Select \`Influencer insta name\` as username,
                            cast(
@@ -37,6 +36,26 @@ async function main() {
                     WHERE category_1 = ?
                        or category_2 = ?
                     order by no_followers desc LIMIT 1;`, category, category, (err: Error, row: any) => {
+                res.send({
+                    "username": row["username"],
+                    "followers": row["no_followers"]
+                });
+            });
+        });
+    })
+
+    // http://localhost:9595/top-influencer-per-country?country=Brazil
+    app.get('/top-influencer-per-country', (req: Request, res: Response) => {
+        let country = req.query['country'] as string;
+        let decodedCountry = decodeURIComponent(country)
+        db.serialize(() => {
+            db.get(`Select \`Influencer insta name\` as username,
+                           cast(
+                                   replace(replace(replace(Followers, '.', ''), 'M', '0000'), 'K', '000')
+                               as INTEGER)           as no_followers
+                    from influencers
+                    WHERE \`Audience country(mostly)\` = ?
+                    order by no_followers desc LIMIT 1;`, decodedCountry, (err: Error, row: any) => {
                 res.send({
                     "username": row["username"],
                     "followers": row["no_followers"]
